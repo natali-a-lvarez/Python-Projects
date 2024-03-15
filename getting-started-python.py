b758e38e889f553eecab52959d59fd8891a372c0
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-03-15 13:26:02 -0400

    creating person class

--- getting-started-python.py
+++ getting-started-python.py
@@ -1,1 +1,14 @@
-print("hello, world")
+# x = 10
+# while x > 0:
+#     print("the value is " + str(x))
+#     x -= 1
+
+# x = 1
+# while x < 10:
+#     print('this is the second value: ' + str(x))
+#     x += 1
+
+input = "Four score and seven years ago"
+for c in range(len(input)):
+  if c in ['a', 'e', 'i', 'o', 'u']:
+    print(c)
