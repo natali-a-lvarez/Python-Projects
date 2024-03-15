6e8e67904039f7516c531763cdb9ca558845f693
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-03-15 13:29:26 -0400

    updating person class

--- person-class.py
+++ person-class.py
@@ -1,8 +1,12 @@
 class Person:
-    def __init__(self,name, age):
+    def __init__(self,name, age, hometown):
         self.name = name
         self.age = age
+        self.hometown = hometown
 
-p1 = Person("nat", 23)
-print(p1.name)
+    def bio(self):
+        print("I am {} and I am {} years old, I grew up in {}.".format(self.name, self.age, self.hometown))
+
+p1 = Person("nat", 23, "bogota")
+print(p1.bio())
 print(p1.age)
